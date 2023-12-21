3151c93d120c5d9c9c0998dda0df1638302d65e3
EnvIr0n <envir0n@proton.me>
2023-12-21 13:58:35 +1300

    Fixed again

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name = 'pyhttpchat-client',
-    version = 'ALPHA 0.1',
+    version = '0.01',
     packages = find_packages(),
     install_requires = requirements,
     author = 'EnvIr0n',

ced0f1302ebb824c2d3a9fc079fb367eb7d1799b
EnvIr0n <envir0n@proton.me>
2023-12-22 00:13:00 +1300

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
    WHAT THE FUCK AM I DOING WITH MY LIFE

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
 
 setup(
-    name = 'pyhttpchat-client',
+    name = 'PyHTTPChat-Client',
     version = '0.01',
     packages = find_packages(),
-    install_requires = requirements,
     author = 'EnvIr0n',
     author_email = 'envir0n@proton.me',
-    url='https://github.com/Env-Ir0n/PYHTTPCHAT-CLIENT.git',
-    scripts= ['pychatclient.py']
+    url='https://github.com/Env-Ir0n/PYHTTPCHAT-CLIENT',
+    scripts= ['src.pychatclient.py']
 
 
 )
